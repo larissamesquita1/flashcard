f188114b5c78663da84854147d04f1b49d7871d1
Larissa Mesquita <larissamesquita168@gmail.com>
2024-11-27 10:38:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,44 @@
+criaCartao(
+    'Serie de jogo',
+    'Quais são as protagonistas da serie Arcane?',
+    'São as irmãs Vi e Jinx(Powder)'
+)
+
+criaCartao(
+    'Saúde mental',
+    'Por que muitas pessoas odeiam a escola?',
+    'muitos estão frustados e exautos de estudar e muitas vezes não concluir seus objetivos.'
+)
+
+criaCartao(
+    'Bem estar',
+    'Como lidar com a ansiedade social?',
+    'terapia e remédios controlados se necessário'
+
+)
+
+criaCartao(
+    'Mente e corpo',
+    'Qual a doença mental mais comum no Brasil?',
+    '86% dos brasileiros sofrem com algum transtorno mental, como a ansiedade e a depressão.'
+)
+criaCartao(
+    'Serie de jogo',
+    'Qual foi o final da 2 temporada de Arcane?',
+    'O show entrega tudo o que prometeu e mais, Jinx se sacrifica mas com a cena final da Caityn vendo que possivelmente ela escapou por um dos dultos de ventilação'
+)
+criaCartao(
+    'Escola',
+    'Por que tantas pessoas odeiam a escola?',
+    'Falta de interesse com matérias, convivío social ruim com colegas de classe e outros'
+)
+criaCartao(
+    'Geografia',
+    'Por que falta respeito entre as outras pessoas no mundo?',
+    'Algumas pessoas não respeitam nada porque não valorizam nada. A gente somente respeita aquilo que nos valorizamos.'
+)
+criaCartao(
+    'Filosofia',
+    'O que significa só sei que nada sei?',
+    'significa um reconhecimento da própria ignorância da parte do autor, não importa quando alguém estude, ele nunca vai saber de tudo.'
+)
